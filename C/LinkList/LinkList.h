18af24e75e6adbdbb1d3ce2522ffbe7e93d420eb
DangHuynh <danghuynh2612@gmail.com>
2017-07-14 18:47:10 +0700

    Declare struct Node and LinkList and add some function

--- C/LinkList/LinkList.h
+++ C/LinkList/LinkList.h
@@ -1,6 +1,22 @@
 #ifndef PRACTICE_CODE_LINKLIST_H
 #define PRACTICE_CODE_LINKLIST_H
 
-// TODO: @dangnhuhuynh will implement
+/**
+ * Struct Node used for LinkList
+ */
+struct Node {
+    int data;
+    Node *pNext;
+};
+
+/**
+ * Struct Link List
+ */
+struct LinkList {
+    Node *pHead;
+    Node *pTail;
+};
+
+
 
 #endif //PRACTICE_CODE_LINKLIST_H
